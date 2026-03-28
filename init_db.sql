df488aebce6167f42e6c422db8452252e14cd869
Virginie Sambou <giniesambou@gmail.com>
2026-03-28 11:24:03 +0000

    Ajout du script de création de la base de données

---
+++ init_db.sql
@@ -0,0 +1,8 @@
+-- 1. Créer la base de données si elle n'existe pas
+CREATE DATABASE IF NOT EXISTS bibliotheque_db;
+
+-- 2. Utiliser cette base pour la suite
+USE bibliotheque_db;
+
+-- (Optionnel) Tu peux aussi y mettre des données de test
+-- INSERT INTO Utilisateur (nom, email) VALUES ('Admin', 'admin@isi.sn');
